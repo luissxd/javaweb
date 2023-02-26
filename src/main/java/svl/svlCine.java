8eb1ab207c286f75869e8b1efc483080df4a30d4
luissxd <luiss.a@hotmail.com>
2023-02-26 18:29:16 -0500

    Se actualiza el servlet svlCine

--- src/main/java/svl/svlCine.java
+++ src/main/java/svl/svlCine.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.List;
 
 import bean.Cine;
-import dao.CineDAO;
+
 
 public class svlCine extends HttpServlet {
 	private static final long serialVersionUID = 1L;
